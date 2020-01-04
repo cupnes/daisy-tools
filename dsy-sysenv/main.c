ee3ee95c35e52069aa6325b83798ccde433363a1
Yuma Ohgami <yuma@ohgami.jp>
2020-01-04 15:00:27 +0900

    add running file.

--- dsy-sysenv/main.c
+++ dsy-sysenv/main.c
@@ -4,10 +4,14 @@
 #include "sysenv.h"
 
 #define WAIT_SEC	1
+#define RUNNING_FILENAME	"running"
 
 void init(void)
 {
 	srand((unsigned int)time(NULL));
+
+	FILE *running_fp = fopen(RUNNING_FILENAME, "a+");
+	fclose(running_fp);
 }
 
 int main(void)
@@ -16,6 +20,12 @@ int main(void)
 
 	while (TRUE) {
 		sysenv_do_cycle();
+
+		FILE *running_fp = fopen(RUNNING_FILENAME, "r");
+		if (running_fp == NULL)
+			break;
+		fclose(running_fp);
+
 		sleep(WAIT_SEC);
 	}
 }
